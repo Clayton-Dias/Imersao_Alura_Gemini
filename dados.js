c68402a7a022d99d798dccc072dc187e0bb3d279
Clayton-Dias <crad13@hotmail.com>
2024-09-07 22:35:30 -0300

    Aula 4
    Aula 4 da Imersão Alura+Gemini AI - Implementando a funcionalidade de busca.

--- dados.js
+++ dados.js
@@ -1,25 +1,30 @@
 let carros = [
     {
+        img: "/img/ac_shelby_cobra.jpg",
         titulo: "AC Shelby Cobra",
         descricao: " Shelby Cobra é, sem dúvida, o carro mais famoso criado por Shelby. Baseado no chassi do AC Ace britânico, o Cobra recebeu um poderoso motor V8 americano, transformando-o em um dos roadsters mais rápidos e emocionantes de sua época.",
         link: "https://pt.wikipedia.org/wiki/AC_Cobra"
     },
     {
+        img: "/img/ford_gt40.jpeg",
         titulo: "Ford GT40",
         descricao: "O Ford GT40 foi desenvolvido por Carroll Shelby para vencer a Ferrari em Le Mans. É um dos carros de corrida mais icônicos da história.",
         link: "https://pt.wikipedia.org/wiki/Ford_GT40"
     },
     {
+        img: "/img/mustang.webp",
         titulo: "Shelby Mustang",
         descricao: "O Shelby Mustang é uma linha de veículos de alto desempenho baseada no Ford Mustang, desenvolvida em parceria com Carroll Shelby.",
         link: "https://pt.wikipedia.org/wiki/Shelby_Mustang"
     },
     {
+        img: "/img/gt350.jpg",
         titulo: "Shelby GT350",
         descricao: "O Shelby GT350 é um modelo de alto desempenho do Mustang, conhecido por sua agilidade e manobrabilidade. Foi que foi construído de 1965 a 1970.",
         link: "https://classiccars.fandom.com/wiki/wiki/Shelby_GT350"
     },
     {
+        img: "/img/gt_500.jpg",
         titulo: "Shelby GT500",
         descricao: "O Shelby GT500 é o modelo mais potente da linha Mustang, com um motor V8 supercarregado, sendo uma homenagem a um ex-piloto automobilístico Carroll Shelby, criador do modelo Cobra.",
         link: "https://pt.wikipedia.org/wiki/Ford_Mustang_Shelby_GT500"
